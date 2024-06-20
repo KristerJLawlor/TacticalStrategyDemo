223d1734fa3f4cc8c09f3592064078c511272217
Krister Joseph Lawlor <kristerjlawlor@gmail.com>
2024-06-20 01:44:07 -0700

    Unit Selection Handled
    Provided a delegate method to broadcast whenever a new unit was selected such that multiple functions can trigger.
    https://medium.com/@apoorvmishra05/using-events-and-delegates-in-unity-and-c-8d73277e280c
    
    The singleton used ensures there are no duplicates of the class instance for UnitActionSystem. The singleton allows global access to the instance by any other script.
    https://medium.com/codex/game-design-pattern-using-singletons-in-unity-acbd05d8ac9d

--- Assets/Scripts/UnitActionSystem.cs
+++ Assets/Scripts/UnitActionSystem.cs
@@ -1,23 +1,46 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class UnitActionSystem : MonoBehaviour
 {
+    //singleton to define instance of this class with public get and private set methods that can be called from any other script
+    //singleton pattern ensures only one instance of its type exists by checking in awake that it has no duplicates
+    public static UnitActionSystem Instance { get; private set; }
 
+    public event EventHandler OnSelectedUnitChanged;  //use delegate to handle event when selected unit is changed
+    
     [SerializeField] private Unit selectedUnit;
     [SerializeField] private LayerMask unitLayerMask;
 
-    private void Update()
+
+    private void Awake()
     {
-        if(TryHandleUnitSelection())
+        //check if Instance has already been set before (we dont want that)
+        if (Instance != null)
         {
+            Debug.LogError("There is more than one ActionSystem!" + transform + " " + Instance);
+            Destroy(gameObject);    //destroy this object to prevent duplicating the pre-existing instance
             return;
         }
+        Instance = this;
+    }
+
+    private void Update()
+    {
 
         if (Input.GetMouseButtonDown(0))
         {
-            selectedUnit.Move(MouseWorld.GetPosition()); //call getposition from class MouseWorld to find where cursor is
+            if (TryHandleUnitSelection())
+            {
+                return;
+            }
+            else
+            {
+                selectedUnit.Move(MouseWorld.GetPosition()); //call getposition from class MouseWorld to find where cursor is
+            }
+           
         }
     }
 
@@ -29,11 +52,29 @@ public class UnitActionSystem : MonoBehaviour
             //if raycast hits unit
             if(raycastHit.transform.TryGetComponent<Unit>(out Unit unit))
             {
-                selectedUnit = unit;
+                SetselectedUnit(unit);
                 return true;
             }
         }
         return false;
+    }
+
+    private void SetselectedUnit(Unit unit)
+    {
+        selectedUnit = unit;
+        //check if there are event subscribers, then call delegate function if true
+        //will fire if selected unit changes
+        //this delegate's broadcast is being listened to in the UnitSelectedVisual script
+        if(OnSelectedUnitChanged != null)
+        {
+            OnSelectedUnitChanged(this, EventArgs.Empty);
+        }
+        //OnSelectedUnitChanged?.Invoke(this, EventArgs.Empty); <-shorthand for above code block    
+    }
+
+    public Unit GetSelectedUnit()
+    {
+        return selectedUnit;    //publicly access the private unit variable
     }
 
 }
--- Assets/Scripts/UnitSelectedVisual.cs
+++ Assets/Scripts/UnitSelectedVisual.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,11 +6,38 @@ using UnityEngine;
 public class UnitSelectedVisual : MonoBehaviour
 {
 
-    [SerializeField] private Unit unit;
-    private MeshRenderer meshRenderer;
+    [SerializeField] private Unit unit; //to understand which unit is currently selected
+    private MeshRenderer meshRenderer;  //will contain the selected visual mesh for the current unit
 
     private void Awake()
     {
         meshRenderer = GetComponent<MeshRenderer>();
+    }
+
+    private void Start()
+    {
+        //subscribe to listen for changes made by using the delegate "OnSelectUnitChanged"
+        //will trigger "UnitactionSystem_OnSelectedUnitChanged" when delegate function is invoked
+        UnitActionSystem.Instance.OnSelectedUnitChanged += UnitActionSystem_OnSelectedUnitChanged;
+
+        UpdateVisual();
+    }
+
+    private void UnitActionSystem_OnSelectedUnitChanged(object sender, EventArgs empty)
+    {
+        UpdateVisual();
+    }
+
+    //update selected unit visual
+    private void UpdateVisual()
+    {
+        if (UnitActionSystem.Instance.GetSelectedUnit() == unit)
+        {
+            meshRenderer.enabled = true;
+        }
+        else
+        {
+            meshRenderer.enabled = false;
+        }
     }
 }
